820ed09410c05cdfe5bd4831559beaa52fdcf6de
pecamu <pe.camuendo.2020@alumnos.urjc.es>
2024-02-19 10:19:14 +0100

    cambios_leer HTML

--- P1/tienda.js
+++ P1/tienda.js
@@ -3,28 +3,15 @@ const http = require('http');
 //-- Definir el puerto a utilizar
 const PUERTO = 9090;
 
-function leerhtml(url)
-{
-    fetch(url)
-    .then(Response=> {
-        if(!Response.ok)
-        {
-            throw new Error('ERROR 404');
-        }
-        return Response.text();
-    })
-    .then(data=> {
-        document.getElementsByTagName('html').innerHTML=data;
-    })
-    .catch(error=>{
-        console.error('ERROR',error);
-    });
-}
+const fs = require('fs');
+
+//-- Fichero a leer
+const FICHERO = 'main.html';
+
 
 
 //-- Crear el servidor
 const server = http.createServer((req, res) => {
-    
   //-- Indicamos que se ha recibido una petición
   console.log("Petición recibida!");
 
@@ -32,11 +19,20 @@ const server = http.createServer((req, res) => {
   //-- cuerpo de la respuesta: Texto plano
   res.setHeader('Content-Type', 'text/html');
 
-  //-- Mensaje del cuerpo
-  res.write('HOLA');
+    fs.readFile(FICHERO, 'utf8', (error, data) => {
 
-  //-- Terminar la respuesta y enviarla
-  res.end();
+        if (error) {  //-- Ha ocurrido algun error
+          console.log("Error!!")
+          console.log(error.message);
+        } 
+        else {  //-- Lectura normal
+              //-- Mensaje del cuerpo
+            res.write(data);
+              //-- Terminar la respuesta y enviarla
+            res.end();
+        }
+    })
+
 });
 
 //-- Activar el servidor: ¡Que empiece la fiesta!
