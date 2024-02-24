de1c6817a962468e6f7d7ec58cc3adb0fbc3a3cb
pecamu <pe.camuendo.2020@alumnos.urjc.es>
2024-02-24 01:12:33 +0100

    cambios servidor

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,69 +1,84 @@
-const http = require('http');
 
 //-- Definir el puerto a utilizar
 const PUERTO = 9090;
 
+// constantes que requiere el back
+const http = require('http');
 const fs = require('fs');
-const { url } = require('inspector');
+const url = require('url');
 
 //-- Fichero a leer
-const FICHERO = 'mouses.html';
+const FICHERO = 'main.html';
 
 
 // crear path o slices
 function info_req(req) {
-
-  // console.log("");
-  // console.log("Mensaje de solicitud");
-  // console.log("====================");
-  // console.log("Método: " + req.method);
-  // console.log("Recurso: " + req.url);
-  // console.log("Version: " + req.httpVersion)
-  // console.log("Cabeceras: ");
-
-  //for (hostname in req.headers)
-    //console.log(`  * ${hostname}: ${req.headers[hostname]}`);
+  // posibles mensajes de consola...
   const myURL = new URL(req.url, 'http://' + req.headers['host']);
-  //console.log("URL completa: " + myURL.href);
-  //console.log("  Ruta: " + myURL.pathname);
+  console.log("URL completa: " + myURL.href);
+  console.log("  Ruta: " + myURL.pathname);
 
-  return myURL
+  return myURL;
 }
   
 
 
 function error404(res){
+  // se puede añadir el codigo 404 y el mensaje error
+  // poner enlace a pagina de error
+  res.writeHead(404,{'Contente-Type':'text/html'});
+  direccion = "error.html";
+  data = fs.readFileSync(direccion);
+  //-- Mensaje del cuerpo            
+  res.write(data);
+  //-- Terminar la respuesta y enviarla
+  res.end();
   console.log("Error!!")
-  console.log(error.message);
 }
-function ok200(res,data){
-              //-- Mensaje del cuerpo
-              res.write(data);
-              //-- Terminar la respuesta y enviarla
-            res.end();
+
+function ok200(res,data,extension){
+  res.writeHead(200,{'Contente-Type':extension});
+  // se puede añadir el codigo 200 y el mensaje ok
+  //-- Mensaje del cuerpo            
+  res.write(data);
+  //-- Terminar la respuesta y enviarla
+  res.end();
 }
 
 //-- Crear el servidor
 const server = http.createServer((req, res) => {
 
-  let url = info_req(req)
-  console.log(url)
-  console.log('PATH',url.pathname)
-  console.log("Esta es la URL solicitada:" + url.href);
-  //-- Indicamos que se ha recibido una petición
-  console.log("Petición recibida!");
+  // lamada a la funcion que toma la informacion del URL
+  let url = info_req(req);
+  let extension = "";
+  let recurso = "";
+  
 
-    fs.readFile(FICHERO, 'utf8', (error, data) => {
+  if (req.method == 'GET'){
 
-        if (error) {  //-- Ha ocurrido algun error
+    if (url.pathname != "/ls"){
+
+      if (url.pathname == "/"){
+        extension += "main.html";
+        recurso += url.pathname.slice(1,);
+      }
+      fs.readFile(recurso, 'utf8', (err, data) => {
+    
+        if (err) {  //-- Ha ocurrido algun error
           error404(res)
         } 
         else {  //-- Lectura normal
-          ok200(res,data)
+          ok200(res,data,extension)
         }
-    })
+      });
+    } 
+    else{
+      ok200(res,data,extension)
+    }
 
+  }
 });
+
 
 //-- Activar el servidor: ¡Que empiece la fiesta!
 server.listen(PUERTO);
